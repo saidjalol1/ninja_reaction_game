17371251239e1b2111b90ad2710a8766e2e850b8
saidjalol1 <turakhujayevsaidjalol06@gmail.com>
2024-02-18 16:07:08 +0500

    finished

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<script>
+import Block from "./components/BlockComp.vue"
+import ResultsCom from "./components/ResultsCom.vue"
+export default {
+  name: 'App',
+  components: { Block, ResultsCom },
+  data(){
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods: {
+    start(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+      console.log(this.delay);
+    },
+    endGame(reactionTime){
+      this.score = reactionTime,
+      this.isPlaying = false,
+      this.showResults = true
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>Ninja reaction timer</h1> 
+    <button @click="start" :disabled="isPlaying">play</button>
+    <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+    <ResultsCom v-if="showResults" :score="score" />
+  </div>
+</template>
+
+<style scoped>
+*{
+  font-family: "Poppins", sans-serif;
+}
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+.wrapper{
+  margin:  0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+button{
+  width: 100px;
+  padding: 10px 10px;
+  font-size: 17px;
+  color: white;
+  background-color: rgb(38, 205, 38);
+  border-radius: 10px;
+  border: none;
+  cursor: pointer;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+</style>
---
+++ src/components/BlockComp.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="block" v-if="isShow" @click="stopTimer">click me</div>
+</template>
+
+<script>
+export default{
+    props: ["delay"],
+    data(){
+        return{
+            isShow : false,
+            timer: null,
+            reactionTime: 0
+        }
+    },
+    mounted(){
+        setTimeout(()=> {
+            this.isShow  = true
+            this.startTimer()
+        }, this.delay)
+    },
+    updated(){
+        console.log("Component Updated");
+    },
+    unmounted(){
+        console.log("unmounted");
+    },
+   methods: {
+    startTimer(){
+        this.timer = setInterval(() => {
+            this.reactionTime += 10
+        }, 10)
+    },
+    stopTimer(){
+        clearInterval(this.timer)
+        console.log(this.reactionTime)
+        this.$emit("end", this.reactionTime)
+    }
+   }
+
+}
+</script>
+
+<style>
+.block{
+    width: 400px;
+    border-radius: 10px;
+    background-color: #0faf87;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+</style>
